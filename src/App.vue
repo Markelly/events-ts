605545f6988b20082c803e60e2d22f3f6ecd12b9
Markelly <markelly.moreira@labtwin.com>
2020-08-21 17:32:05 +0200

    app margin and alingment

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,8 @@ export default class App extends Vue {}
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  max-width: 1272px;
+  margin: 0 auto;
+  padding: 0 24px;
 }
 </style>
